01a1589beb614504699723ebdb8a2fba6c21f9fc
etiiizel <franciscoetizel@gmail.com>
2025-12-15 15:49:25 -0300

    script-watch_change

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
-  return res.end('Test Servergit');
+  return res.end('Test Server');
 });
 
-server.listen(3333);
+server.listen(3333, () => {
+  console.log('🚀 Servidor iniciado na porta 3333');
+});
